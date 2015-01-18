8ee455e937194c9ad4c45a0742371758f3ac86b9
longtaoge <61852263@qq.com>
2015-01-18 23:55:48 +0800

    测试通过

--- sample/src/main/java/com/yalantis/pulltorefresh/sample/PullToRefreshActivity.java
+++ sample/src/main/java/com/yalantis/pulltorefresh/sample/PullToRefreshActivity.java
@@ -17,6 +17,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 测试通过
+ */
 public class PullToRefreshActivity extends ActionBarActivity {
 
     public static final int REFRESH_DELAY = 2000;
